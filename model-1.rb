004bc6934de8dd0b82e595742a7eccc1d8ee4ca9
Jessica Grazulis <jlgrazulis@gmail.com>
2021-08-31 14:27:24 -0600

    Add bully breeds

--- model-1.rb
+++ model-1.rb
@@ -1,6 +1,7 @@
-dog_types = american bulldog; husky; beagle
+dog_breed_examples = american bulldog; husky; beagle
 bully_breeds = 4
 AKC_dog_breeds = 190
 american_bulldog = true
-staffordshire terrier = true
+staffordshire_terrier = true
 labradoodle = false
+bully_breeds = ['american_bulldog', 'staffordshire_terrier', 'pit_bull_terrier', 'bull_terrier']
